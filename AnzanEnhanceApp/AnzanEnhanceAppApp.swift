f6bf43e77bdef1219ab0c95258acb5de82b64012
jin-kirino <jinkirino@gmail.com>
2022-10-27 15:41:10 +0900

    first push

---
+++ AnzanEnhanceApp/AnzanEnhanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnzanEnhanceAppApp.swift
+//  AnzanEnhanceApp
+//
+//  Created by 神　樹里乃 on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct AnzanEnhanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnzanEnhanceApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnzanEnhanceApp
+//
+//  Created by 神　樹里乃 on 2022/10/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
