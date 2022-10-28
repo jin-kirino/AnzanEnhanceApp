396b2f4ba42dff7b5f52a1907194b81188bc8f62
jin-kirino <jinkirino@gmail.com>
2022-10-28 23:45:39 +0900

    AnswerViewを作成

---
+++ AnzanEnhanceApp/BackgoundView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgoundView.swift
+//  AnzanEnhanceApp
+//
+//  Created by 神　樹里乃 on 2022/10/27.
+//
+
+import SwiftUI
+
+struct BackgoundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgoundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgoundView()
+    }
+}
---
+++ AnzanEnhanceApp/SoundPlayer.swift
@@ -0,0 +1,13 @@
+//
+//  SoundData.swift
+//  AnzanEnhanceApp
+//
+//  Created by 神　樹里乃 on 2022/10/28.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundData: NSObject {
+    let correct
+}
